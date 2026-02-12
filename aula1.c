94a005265a249279f4fc3b2e5943a261f8abf5d2
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-12 10:22:13 -0300

    atualizando

--- aula1.c
+++ aula1.c
@@ -1,6 +1,9 @@
+/* include é para incluir
+<studio.h> é a biblioteca que contém funções da linguagem C */
+
 #include <stdio.h>
 
 int main(){
-    printf("hello, world!");
+    printf("hello, world!\n");
     return 0;
 }
