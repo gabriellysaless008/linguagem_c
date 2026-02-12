bbcc559a732a584c34433119ebdc9e3bb2e22af6
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-12 10:26:49 -0300

    atualizando 0.2

--- aula1.c
+++ aula1.c
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 
-int main(){
-    printf("hello, world!\n");
-    return 0;
+/* int main é a função principal */
+int main(){ // tem que abrir a funçãi { chaves } e tudo que estiver entre elas,
+            //a função principal vai ser executado no programa.
+
+    // "printf" é para imprimir
+    printf("hello, world!\n"); // \n é para pular linha
+    return 0; //"return 0" se tiver tudo certo no programa vai retornar 0 e rodar
+              //se algo estiver errado vai retornar 1
 }
