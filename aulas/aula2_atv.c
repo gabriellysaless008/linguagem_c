c9c1c33925599486c9a69ce1c2e3b6e5440d97ba
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-12 11:33:48 -0300

    atualizando 0.4 + atividade

---
+++ aulas/aula2_atv.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int zero = 0;
+    float pi = 3.14;
+    char fruta = 'U'; 'V'; 'A';
+    bool verdadeiro = true;
+}
+
+
+
+
+
+/* Exercicio 1
+a)Declarar um tipo de cada variáveis e atribuir o nome que vocês quiserem.
+b)Imprimir as variáveis.
+c)Declarar 3 variavéis do mesmo tipo em uma linha e imprimi-las uma ao lado da outra.
+d)Declarar uma variável em uma linha e atribuir o valor em outra e imprimi-la.
+e)Escrever seu nome.
+*/
