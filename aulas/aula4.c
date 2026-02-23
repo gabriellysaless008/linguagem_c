9aed3dfc233087ebad15cacd17aad81cf3092717
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-23 08:53:08 -0300

    aula 4

---
+++ aulas/aula4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+ char letra;
+ int idade;
+ float altura;
+
+ printf("Digite a primeira letra do seu nome: ");
+ //dar espaço antes do "%c" -> " %c" para limpar o buffer
+ scanf(" %c", &letra);//escaneando um caractere
+ printf("Digite sua idade: ");
+ scanf(" %d", &idade);
+ printf("Digite sua altura: ");
+ scanf(" %f", &altura);
+
+ printf("Primeira letra do seu nome -> %c\n", letra);
+ printf("Sua idade -> %d\n", idade);
+ printf("Sua altura -> %.2f\n", altura);
+ 
+ return 0;
+    
+}
