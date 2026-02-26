e0f2201268d1fb228cbe497add00d2fdee2d7f00
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-26 11:27:43 -0300

    faltou a 11, 10 e 9

--- aulas/lista2.c
+++ aulas/lista2.c
@@ -3,8 +3,63 @@
 #include <stdio.h>
 
 int main(){
-    printf ("Exercicio 1\n");
+    printf ("# Exercicio 1 #\n");
     int n = 90;
-    printf("O numero e: %d", n);
+    printf("O numero e: %d\n\n", n);
+
+    printf ("# Exercicio 2 #\n");
+    int n1 = 40, n2 = 60;
+    int soma = n1 + n2;
+    printf("%d + %d = %d\n\n", n1, n2, soma);
+
+    printf ("# Exercicio 3 # \n");
+    float n3 = 3.14, n4 = 1.24;
+    float subtracao = n3 - n4;
+    printf("%.2f - %.2f = %.2f\n\n", n3, n4, subtracao);
+
+    printf ("# Exercicio 4 # \n");
+    int n5 = 9, n6 = 5;
+    int multiplicacao = n5 * n6;
+    printf("%d * %d = %d\n\n", n5, n6, multiplicacao);
+
+    printf ("# Exercicio 5 # \n");
+    float n7 = 9.00, n8 = 6.00;
+    float divisao = n7 / n8;
+    printf("%.2f / %.2f = %.2f\n\n", n7, n8, divisao);
+
+    printf ("# Exercicio 6 # \n");
+    int dobro;
+    printf("Digite um numero:  \n");
+    scanf("%d", &dobro);
+    int mult = dobro * 2;
+    printf("o dobro do numero e: %d\n\n", dobro * 2);
+
+    printf ("# Exercicio 7 # \n");
+    char letra1 = 'M';
+    printf("Letra: %c\n\n", letra1);
+
+    printf ("# Exercicio 8 # \n");
+    float num;
+    printf("Digite um numero ex(20.5):  \n");
+    scanf("%2.1f", &num);
+    float adicao = num + 10.5;
+    printf("%2.1f + %2.1f = %2.1f\n\n", adicao + 10.5);
+    printf("a soma foi de: %2.1f\n\n", adicao + 10.5);
+
+    printf ("# Exercicio 9 # \n");
+    int n9 = 100 , n10 = 80, n11 = 60 , n12 = 90 ;
+    float media = n9 + n10 + n11 + n12 / 4;
+    printf("A media foi: %2.1f", media);
+
+    printf ("# Exercicio 10 # \n");
+
+
+
+
+
+
+
+
+    
 
 }
