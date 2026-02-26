e18e70112cef255acc20c385f903351635966675
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-02-26 14:42:58 -0300

    lista concluida

--- aulas/lista2.c
+++ aulas/lista2.c
@@ -50,7 +50,7 @@ int main(){
     printf ("# Exercicio 9 # \n");
     int n9 = 100 , n10 = 80, n11 = 60 , n12 = 90;
     float media = (n9 + n10 + n11 + n12) / 4;
-    printf("A media foi: %.1f", media);
+    printf("A media foi: %.1f\n\n", media);
 
     printf ("# Exercicio 10 # \n");
     int numero1, numero2;
@@ -61,13 +61,28 @@ int main(){
     int sub = numero1 - numero2;
     printf("A diferenca entre os numeros sao: %d\n\n", sub);
 
-
+    printf ("# Exercicio 11 # \n");
+     int mult1 = 5 * 1;
+    int mult2 = 5 * 2;
+    int mult3 = 5 * 3;
+    int mult4 = 5 * 4;
+    int mult5 = 5 * 5;
+    int mult6 = 5 * 6;
+    int mult7 = 5 * 7;
+    int mult8 = 5 * 8;
+    int mult9 = 5 * 9;
+    int mult10 = 5 * 10;
+    printf("Tabuada do 5\n");
+    printf("5 x 1 = %d\n", mult1);
+    printf("5 x 2 = %d\n", mult2);
+    printf("5 x 3 = %d\n", mult3);
+    printf("5 x 4 = %d\n", mult4);
+    printf("5 x 5 = %d\n", mult5);
+    printf("5 x 6 = %d\n", mult6);
+    printf("5 x 7 = %d\n", mult7);
+    printf("5 x 8 = %d\n",mult8);
+    printf("5 x 9 = %d\n", mult9);
+    printf("5 x 10 = %d\n", mult10);
 
     return 0;
-
-
-
-
-    
-
 }
