580960aba1eae259a5c142bebd20ab0247b43d92
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-03-02 09:17:15 -0300

    aula 5

--- aulas/aula5.c
+++ aulas/aula5.c
@@ -1,7 +1,21 @@
-
+// aula #5 - operadores logicos, relacionais e if else // 
 #include <stdio.h>
 
 int main(){
-    
+    int x, y;
+    printf ("Digite o valor de x: ");
+    scanf ("%d", &x);
+    printf ("Digite o valor de y: ");
+    scanf ("%d", &y);
+    if(x > y){
+        printf("O valor de x e maior que o valor de y\n");
+
+    }else if(x < y){
+        printf("O valor de y e maior que o valor de x\n");
+    } else {
+        printf("Os valores d x e y sao iguais");
+    }
+
+    return 0;
 
 }
