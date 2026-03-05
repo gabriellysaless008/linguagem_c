a38fd033e635f170086d2f1ec702d123c2e00020
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-03-05 11:13:05 -0300

    lista 3

---
+++ aulas/lista_3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+    int main(){
+    printf( "Exercicio 1\n\n" );
+    int n1, n2;
+    printf( "Insira o primeiro numero: " );
+    scanf( "%d", &n1 );
+    printf( "Insira o segundo numero: " );
+    scanf( "%d", &n2 );
+    if ( n1 > n2 ){
+        printf( "O primeiro numero e maior \n" );
+    }else{
+      printf ( "O segundo numero e maior \n\n" );
+}
+
+    printf( "Exercicio 2\n\n" );
+    int senha;
+    printf("Antes de proseguir, insira sua senha:  ");
+    scanf("%d", &senha);
+    if (senha == 1234){
+        printf("Acesso permitido!");
+    }else{
+        printf("Acesso negado, tente outra senha!\n\n");
+    }
+
+    printf( "Exercicio 3\n\n" );
+    int dtnasc;
+    printf("Digite seu ano de nascimento: ");
+    scanf("%d", &dtnasc);
+    if ( dtnasc <= 2008 ){
+        printf( "E maior de idade! \n" );
+    }else{
+        printf ( "Nao atingiu a maioridade! \n\n" );
+    }
+
+    printf( "Exercicio 4\n\n" );
+    int nlaranjas;
+    printf("Digite o numero de laranjas compradas: ");
+    scanf("%d", &nlaranjas);
+
+    if (nlaranjas <= 12){
+        printf("O total da compra foi:  %.2f", nlaranjas * 0.30);
+    }else{
+        printf("O total da compra foi:  %.2f\n\n", nlaranjas * 0.25);
+    }
+
+    }
+
+
---
+++ aulas/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){ 
+printf( "Exercicio 4\n\n" );
+    int nlaranjas;
+    printf("Digite o numero de laranjas compradas: ");
+    scanf("%d", &nlaranjas);
+
+    if (nlaranjas <= 12){
+        printf("O total da compra foi:  %.2f", nlaranjas * 0.30);
+    }else{
+        printf("O total da compra foi:  %.2f", nlaranjas * 0.25);
+    }
+
+    }
