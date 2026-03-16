48c00b77458e62fec0e94a3becd25832ca32d6a6
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-03-16 09:32:13 -0300

    lista não completa

---
+++ aulas/lista_4.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Exercicio 1\n\n");
+    int dia_semana;
+    printf("Digite o numero do dia semana: \n");
+    scanf("%d", &dia_semana);
+
+    switch(dia_semana){
+        case 1:
+            printf("Domingo\n");
+            break;
+        case 2:
+            printf("Segunda-feira\n");
+            break;
+        case 3:
+            printf("Terca-feira\n");
+            break;
+        case 4:
+            printf("Quarta-feira\n");
+            break;
+        case 5:
+            printf("Quinta-feira\n");
+            break;
+        case 6:
+            printf("Sexta-feira\n");
+            break;
+        case 7:
+            printf("Sabado\n");
+            break;
+        default:
+            printf ("Error!!!\n");
+            break;
+
+    }
+
+    printf("Exercicio 2\n\n");
+    int mes_ano;
+    printf("Digite um mes do ano: ");
+    scanf("%d", &mes_ano);
+        
+        switch(mes_ano){
+            case 1: 
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                printf("Meses com 31 dias\n");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                printf("Meses com 30 dias\n");
+                break;
+            case 2:
+                printf("Mes com 28 ou 29 dias");
+                break;
+            default:
+                printf ("Error!!!\n");
+                break;
+
+        }
+
+    printf("Exercicio 3\n\n");
+    int codigo_lanche;
+    printf("---Ola, seja bem vindo ao Dois Hermanos lanches!!---\n\n");
+    printf("# -- Cardapio Dois Hermanos lanches -- #\n\n");
+    printf("# 1 - XSalada\n");
+    printf("# 2 - XBacon\n");
+    printf("# 3 - XBacon Egg\n");
+    printf("# 4 - XTudao\n");
+    printf("# 5 - Dogao simples\n");
+    printf("# 6 - Dogao completo\n\n");
+    printf("Digite o numero do lanche desejado: ");
+    scanf("%d", &codigo_lanche);
+    switch(codigo_lanche){
+        case 1:
+            printf ("XSalada!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 19.00\n");
+            printf("Ingredientes: Hamburguer, Salada, Presunto, Queijo, Batata.\n");
+            break;
+        case 2:
+            printf ("XBacon!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 25.00\n");
+            printf("Ingredientes: Hamburguer, Bacon, Presunto, Queijo, Batata, Alface, Tomate, Milho.\n");
+            break;
+        case 3:
+            printf ("XBacon Egg!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 27.00\n");
+            printf("Ingredientes: Hamburguer, Bacon, Ovo, Presunto, Queijo, Batata, Alface, Tomate, Milho.\n");
+            break;
+        case 4:
+            printf ("XTudao!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 34.00\n");
+            printf("Ingredientes: Hamburguer, Bacon, Calabresa, Frango, Ovo, Presunto, Queijo, Batata, Alface, Tomate, Milho.\n");
+            break;
+        case 5:
+            printf ("Dogao simples!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 17.00\n");
+            printf("Ingredientes: Molho, Salshicha, Milho, Alface, Batata.\n");
+            break;
+        case 6:
+            printf ("Dogao Loko!!\n");
+            printf("Fez uma otima escolha, segue as informacoes do produto: \n");
+            printf("Preco, R$: 24.00\n");
+            printf("Ingredientes: Molho, Salshicha, Carne, Frango, Bacon, Queijo, Caabresa, Catupiry, Milho, Alface, Batata.\n");
+            break;
+    }
+
+
+        return 0;
+    }
+    
--- aulas/main.c
+++ aulas/main.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
 int main(){ 
-printf( "Exercicio 10\n\n" );
-    int idade;
-    printf("Insira sua idade:  ");
-    scanf ("%d", &idade);
-    if (idade <= 17){
-        printf ("E um bebe ainda");
-    }else if (idade <= 59){
-        printf ("E adulto ainda");
-    }else{
-        printf ("Ta velho ja");
-    }
+     printf("Exercicio 2\n\n");
+    int mes_ano;
+    printf("Digite um mes do ano: ");
+    scanf("%d", &mes_ano);
+        
+        switch(mes_ano){
+            case 1: 
+                printf("Janeiro\n");
+            case 3:
+                printf("Marco\n");
+            case 5:
+                printf("Maio\n");
+            case 7:
+                printf("Julho\n");
+            case 8:
+                printf("Agosto\n");
+            case 10:
+                printf("Outubro\n");
+            case 12:
+                printf("Dezembro\n");
+                printf("Meses com 31 dias\n");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                printf("Meses com 30 dias\n");
+                break;
+            case 2:
+                printf("Mes com 28 ou 29 dias");
+                break;
+            default:
+                printf ("Error!!!\n");
+                break;
 
-
-
+        }
 }
