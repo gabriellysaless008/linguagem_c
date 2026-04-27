995152f25f91635f0792ee522319da61a31a42e2
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-04-27 09:30:14 -0300

    fazendo a lista

--- aulas/lista_7.c
+++ aulas/lista_7.c
@@ -28,6 +28,49 @@ int main(){
     for(int intervalo = n_ini; intervalo >= n_fim; intervalo--){
         printf("%d \n", intervalo);
     }
+
+       
+    printf("Exercicio 4\n\n");
+    int valor, valor_maior = 0;
+    for(int i = 1; i <= 10; i++){
+        printf("Digite um valor: ");
+        scanf("%d", &valor);
+        if (valor >= valor_maior){
+            valor_maior = valor;
+        }
+    } 
+        printf("Valor maior: %d", valor_maior);
+
+    printf("Exercicio 5\n\n");
+    int idade, idade_maior = 0;
+    for(int i = 1; i <= 10; i++){
+        printf("Digite sua idade: ");
+        scanf("%d", &idade);
+        if (idade >= 18){
+            idade_maior++;
+        }
+    } 
+        printf("Pessoas maiores de idade: %d", idade_maior);
+
+    printf("Exercicio 6\n\n");
+    int n_patinhos = 0;
+    printf("Quantos patinhos a mamae pata tem?: ");
+    scanf("%d", &n_patinhos);
+    for(int p = n_patinhos; p >= 0; p--){
+        if (p >= 1){
+            printf("%d patinhos foram passear\n", p);
+            printf("Alem das montanhas\n");
+            printf("Para brincar\n");
+            printf("A mamae gritou: Qua, qua, qua, qua\n");
+            printf("Mas so %d patinhos voltaram de la\n\n", p - 1);
+        }else{
+            printf("A mamae patinha foi procurar\n");
+            printf("Alem das montanhas, na beira do mar\n");
+            printf("A mamae gritou: Qua, qua, qua, qua!\n");
+            printf("E os cinco patinhos voltaram de la\n\n");
+        }
+
+    }
      
     return 0;
 }
--- aulas/main.c
+++ aulas/main.c
@@ -2,14 +2,29 @@
 
 int main(){
    
-    printf("Exercicio 4\n\n");
-    int valor, valor_maior;
-    for(int v = 1; v <= 10; v++){
-        printf("Digite um valor: ");
-        scanf("%d", &valor);
+    printf("Exercicio 6\n\n");
+    int n_patinhos = 0;
+    printf("Quantos patinhos a mamae pata tem?: ");
+    scanf("%d", &n_patinhos);
+    for(int p = n_patinhos; p >= 0; p--){
+        if (p >= 1){
+            printf("%d patinhos foram passear\n", p);
+            printf("Alem das montanhas\n");
+            printf("Para brincar\n");
+            printf("A mamae gritou: Qua, qua, qua, qua\n");
+            printf("Mas so %d patinhos voltaram de la\n\n", p - 1);
+        }else{
+            printf("A mamae patinha foi procurar\n");
+            printf("Alem das montanhas, na beira do mar\n");
+            printf("A mamae gritou: Qua, qua, qua, qua!\n");
+            printf("E os cinco patinhos voltaram de la\n\n");
+        }
+
     }
 
-   
+
+
+
 
 }
     
