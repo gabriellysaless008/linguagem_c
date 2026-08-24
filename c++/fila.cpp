49dc196fb488bf0e1d7663fca324f9cc8968f261
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-08-24 08:26:29 -0300

    vendo sobre fila

---
+++ c++/fila.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <queue>
+
+// Fila(queue) é ma estrutura que sgue uma regra.
+// FIFO - first in first out.
+// fila -> o primeiro a entrar é o primeiro a sair.
+
+// push() -> adiciona um elemnto no final da fila.
+// pop() -> remove o primeiro elemento da fila.
+// front() -> consulta o primeiro elemento.
+// back() -> consukta o último elemento.
+// empty() -> verifica se a fila está vazia
+// size() -> retorna a quantidade de elementos.
+
+
+int main(){
+    char vetor[5] = {'B', 'C', 'D', 'E', 'F'};
+
+    return 0;
+}
